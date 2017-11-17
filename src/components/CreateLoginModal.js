8c4737b64eb87ccfc6fb431911fc8ca790bdb184
ivanjonas <ivanjonasgomes@gmail.com>
2017-11-16 22:52:32 -0500

    fix: reinstate Create Modal feedback messages

--- src/components/CreateLoginModal.js
+++ src/components/CreateLoginModal.js
@@ -13,7 +13,7 @@ export default class CreateLoginModal extends React.Component {
           <input type="text" name="username" placeholder="username"/>
           <input type="text" name="password" placeholder="password"/>
           <input type="submit" value="Create"/>
-          {this.props.editLoginModalErrorMessage && <p>{this.props.editLoginModalErrorMessage}</p>}
+          {this.props.createLoginModalErrorMessage && <p>{this.props.createLoginModalErrorMessage}</p>}
         </form>
       </Modal>
     )
