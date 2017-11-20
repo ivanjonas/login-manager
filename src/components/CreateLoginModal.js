34433ed6ece18d03aad0a6690f6af7abc230f520
Ivan Jonas <ivan@jonas.ninja>
2017-11-20 18:53:24 -0500

    in-progress from dying laptop

--- src/components/CreateLoginModal.js
+++ src/components/CreateLoginModal.js
@@ -1,21 +1,53 @@
 import React from 'react'
 import Modal from 'react-modal'
 
-export default class CreateLoginModal extends React.Component {
+export default class CreateLoginButton extends React.Component {
+  state = {
+    isCreateLoginModalOpen: false,
+    createLoginModalErrorMessage: undefined
+  }
+
+  handleCreateLogin = (e) => {
+    e.preventDefault()
+    const username = e.target.elements.username.value.trim()
+    const password = e.target.elements.password.value.trim()
+    const success = this.props.createLogin(username, password)
+
+    if (success) {
+      this.setState(() => ({
+        isCreateLoginModalOpen: false,
+        createLoginModalErrorMessage: undefined
+      }))
+    } else {
+      // show an error message to the user
+      this.setState(() => ({
+        createLoginModalErrorMessage: 'The Login you are trying to create already exists.'
+      }))
+    }
+
+    return false
+  }
+
   render() {
     return (
-      <Modal 
-        isOpen={this.props.isOpen}
-        contentLabel="Modal to Create a New Login"
-        className="CreateLoginModal"
-      >
-        <form onSubmit={this.props.handleCreateLogin}>
-          <input type="text" name="username" placeholder="username"/>
-          <input type="text" name="password" placeholder="password"/>
-          <input type="submit" value="Create"/>
-          {this.props.createLoginModalErrorMessage && <p>{this.props.createLoginModalErrorMessage}</p>}
-        </form>
-      </Modal>
+      <div>
+        <button onClick={(e) => {
+          this.setState(() => ({ isCreateLoginModalOpen: true }))
+        }}>Create New Login</button>
+        <Modal
+          isOpen={this.state.isCreateLoginModalOpen}
+          contentLabel="Modal to Create a New Login"
+          handleCloseModal={}
+          shouldCloseOnOverlayClick={true}
+        >
+          <form onSubmit={this.handleCreateLogin}>
+            <input type="text" name="username" placeholder="username" />
+            <input type="text" name="password" placeholder="password" />
+            <input type="submit" value="Create" />
+            {this.state.createLoginModalErrorMessage && <p>{this.state.createLoginModalErrorMessage}</p>}
+          </form>
+        </Modal>
+      </div>
     )
   }
 }
